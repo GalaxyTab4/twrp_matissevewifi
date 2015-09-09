2ab78375f819c72b3a408986dc420d46c8a8a958
Patrik Zakhar <123patokt@gmail.com>
2015-09-09 17:11:07 +0200

    a31ul: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+TWRP device tree for HTC Desire 620 (a31ul)
+========================================================
+
+For building TWRP for MSM8916 models only.
+
+Known bugs:
+* offmode charging (turns on TWRP UI instead of the charging image)
---
+++ omni_a31ul.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/htc/a31ul/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a31ul
+PRODUCT_NAME := omni_a31ul
+PRODUCT_BRAND := htc
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_MODEL := HTC Desire 620
